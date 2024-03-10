0d1ffdc3fdeea962bd606988ff560fbfb968d0e9
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-10 19:06:43 -0400

    removed duplicate route, added login, logout route handled by UserController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +15,7 @@ use App\Http\Controllers\ExampleController;
 Route::get('/', [ExampleController::class, 'homePage']);
 
 Route::get('/', [UserController::class, 'showCorrectHomePage']);
-Route::get('/about', [ExampleController::class, 'aboutPage']);
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
