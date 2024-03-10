be715f985511fba013c561958b5e1843380e1ecb
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-10 18:42:11 -0400

    user authentication and homepage logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,37 @@
 <?php
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+
+    public function showCorrectHomePage() {
+        if (auth()->check()) {
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
+    }
+
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required',
+        ]);
+
+        if (auth()->attempt([
+            'username' => $incomingFields['loginusername'],
+            'password' => $incomingFields['loginpassword'],
+        ])) {
+            $request->session()->regenerate();
+            return 'Congrats!!!';
+        } else {
+            return 'Sorry!!!';
+        }
+    }
+
     public function register(Request $request) {
         $incomingFields = $request->validate([
             'username' => [
@@ -26,11 +51,8 @@ class UserController extends Controller
                 'confirmed'
             ],
         ]);
-
         $incomingFields['password'] = bcrypt($incomingFields['password']);
-
         User::create($incomingFields);
-
         return 'Hello from register function';
     }
 }
