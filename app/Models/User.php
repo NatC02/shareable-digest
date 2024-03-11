968fc3188162705c0427e908dca13cc0154f7679
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-11 10:17:50 -0400

    Enable efficient retrieval of user followers and following information

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function followers() {
+        return $this->hasMany(Follow::class, 'followeduser', 'id');
+    }
+
+    public function followingTheseUsers() {
+        return $this->hasMany(Follow::class, 'user_id', 'id');
+    }
+
     public function posts() {
         return $this->hasMany(Post::class, 'user_id');
     }
