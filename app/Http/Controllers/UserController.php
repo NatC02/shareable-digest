e154fa44b63827a3ab33b37af1b7e25651ee268a
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-11 10:55:20 -0400

    Implement pagination for following list in profile view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,7 +84,7 @@ class UserController extends Controller
                     return view(
                         'homepage-feed',
                         [
-                            'posts' => auth()->user()->feedPosts()->latest()->get()
+                            'posts' => auth()->user()->feedPosts()->latest()->paginate(4)
                         ]
                     );
                 }
