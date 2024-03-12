54448ac37120f56a1286554968f951bee51a38b6
Natan Ceballos <natan.ceballos@tutanota.com>
2024-03-11 21:30:01 -0400

    Implement post update functionality with validation, API endpoints for post deletion and creation, and minor adjustments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use App\Mail\NewPostEmail;
+use App\Jobs\SendNewPostEmail;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
-
 class PostController extends Controller
 {
     public function search(string $term) {
@@ -15,16 +11,29 @@ class PostController extends Controller
         $posts->load('user:id,username,avatar');
         return $posts;
     }
-    
-    public function delete(Post $post) {
-        if (auth()->user()->cannot('delete', $post)) {
-            return 'You cannot do that';
-        }
-        $post->delete();
-
-        return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
+    public function actuallyUpdate(Post $post, Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $post->update($incomingFields);
+        return back()->with('success', 'Post successfully updated.');
+    }
+    public function showEditForm(Post $post) {
+        return view('edit-post', ['post' => $post]);
     }
 
+    public function deleteApi(Post $post) {
+        $post->delete();
+        return 'true';
+    }
+
+    public function delete(Post $post) {
+        $post->delete();
+        return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
+    }
     public function viewSinglePost(Post $post) {
         $ourHTML = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><u>');
         $post['body'] = $ourHTML;
@@ -34,7 +43,7 @@ class PostController extends Controller
         );
     }
 
-    public function storeNewPost(Request $request) {
+    public function storeNewPostApi(Request $request) {
         $incomingFields = $request->validate([
             'title' => 'required',
             'body' => 'required',
@@ -52,9 +61,25 @@ class PostController extends Controller
             'title' => $newPost->title,
         ]));
 
-        return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created!');
+        return $newPost->id;
     }
 
+    public function storeNewPost(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+        $newPost = Post::create($incomingFields);
+        dispatch(new SendNewPostEmail([
+            'sendTo' => auth()->user()->email,
+            'name' => auth()->user()->username,
+            'title' => $newPost->title,
+        ]));
+        return redirect("/post/{$newPost->id}")->with('success', 'New post successfully created!');
+    }
     public function showCreateForm() {
         return view('create-post');
     }
